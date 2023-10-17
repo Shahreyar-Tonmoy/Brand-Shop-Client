62c5402ef716b8d09ac756f155813a28982d630b
Shahreyar Tonmoy <shahreyartonmoy001@gmail.com>
2023-10-17 20:31:25 +0600

    first commit

---
+++ src/Router/Router.jsx
@@ -0,0 +1,23 @@
+import { createBrowserRouter } from "react-router-dom";
+
+
+
+const Router = createBrowserRouter([
+    // {
+    //     path :"/",
+    //    element :,
+    //    errorElement :,
+    //    children :[
+    //     {
+    //         path : "/",
+    //         element : <Home></Home>,
+           
+    //     },
+        
+        
+    //    ]
+       
+    // }
+])
+
+export default Router;
---
+++ src/main.jsx
@@ -0,0 +1,16 @@
+/* eslint-disable no-undef */
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import {
+
+  RouterProvider,
+} from "react-router-dom";
+
+import './index.css'
+import Router from './Router/Router';
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <RouterProvider router={Router} />
+  </React.StrictMode>,
+)
